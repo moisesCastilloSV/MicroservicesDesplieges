5a443cadc4be0589992a770d4f81b6ae9264cde1
javixcm <javixcm@gmail.com>
2021-02-12 17:11:20 -0500

    Changes 2

--- Servicios.api.Gateway/Startup.cs
+++ Servicios.api.Gateway/Startup.cs
@@ -23,7 +23,6 @@ namespace Servicios.api.Gateway
         {
             Configuration = configuration;
         }
-
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
